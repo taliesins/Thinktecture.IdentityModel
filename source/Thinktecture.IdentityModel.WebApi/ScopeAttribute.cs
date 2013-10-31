eda4de2c00a2e0f61658b363813560821a6b6492
Dominick Baier <dbaier@leastprivilege.com>
2013-10-31 10:06:59 +0100

    Added 1st batch of Web API files

---
+++ source/Thinktecture.IdentityModel.WebApi/ScopeAttribute.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Thinktecture.IdentityModel.WebApi
+{
+    public class ScopeAttribute : AuthorizeAttribute
+    {
+        string[] _scopes;
+        static string _scopeClaimType = "scope";
+
+        public static string ScopeClaimType 
+        {
+            get { return _scopeClaimType; }
+            set { _scopeClaimType = value; }
+        }
+
+        public ScopeAttribute(params string[] scopes)
+        {
+            if (scopes == null)
+            {
+                throw new ArgumentNullException("scopes");
+            }
+
+            _scopes = scopes;
+        }
+
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            var grantedScopes = ClaimsPrincipal.Current.FindAll(_scopeClaimType).Select(c => c.Value).ToList();
+
+            foreach (var scope in _scopes)
+            {
+                if (!grantedScopes.Contains(scope))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
