84c702104299f01ce446789de1e3ba385804c571
Dominick Baier <dbaier@leastprivilege.com>
2013-10-31 16:30:35 +0100

    Added JWT handler

---
+++ source/Thinktecture.IdentityModel.WebApi/JwtSecurityTokenHandlerWrapper.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see LICENSE
+ */
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace Thinktecture.IdentityModel.Tokens
+{
+    class IdentityModelJwtSecurityTokenHandler : JwtSecurityTokenHandler
+    {
+        TokenValidationParameters validationParams;
+
+        public IdentityModelJwtSecurityTokenHandler(TokenValidationParameters validationParams, Dictionary<string, string> inboundClaimTypeMap = null)
+        {
+            this.validationParams = validationParams;
+
+            if (inboundClaimTypeMap != null)
+            {
+                InboundClaimTypeMap = inboundClaimTypeMap;
+            }
+        }
+
+        public override ReadOnlyCollection<ClaimsIdentity> ValidateToken(SecurityToken token)
+        {
+            var jwt = token as JwtSecurityToken;
+            var list = new List<ClaimsIdentity>(this.ValidateToken(jwt, validationParams).Identities);
+            return list.AsReadOnly();
+        }
+    }
+}
