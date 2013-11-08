abd918d60921589a573a2a21551ce0401a2edfd6
Dominick Baier <dbaier@leastprivilege.com>
2013-11-08 13:26:22 +0100

    Added HTTP helpers to Core

---
+++ source/Thinktecture.IdentityModel.Core/Http/ThumbprintSet.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * Contribution by Pedro Felix
+ * see LICENSE
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Thinktecture.IdentityModel.Http
+{
+    public class ThumbprintSet : HashSet<string>
+    {
+        public ThumbprintSet(params string[] thumbprints)
+            : base(thumbprints, StringComparer.OrdinalIgnoreCase)
+        { }
+    }
+}
