e716964477313afdf6327f48ca0723127f57180b
Dominick Baier <dbaier@leastprivilege.com>
2013-11-12 11:12:37 +0100

    Added embedded RO flow sample

---
+++ samples/Client/EmbeddedAuthorizationServer/EmbeddedAuthorizationServer/Provider/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EmbeddedAuthorizationServer.Provider
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            // accept any client
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // validate user credentials
+            if (context.UserName != context.Password)
+            {
+                context.Rejected();
+                return;
+            }
+
+            // create identity
+            var id = new ClaimsIdentity("Embedded");
+            id.AddClaim(new Claim("sub", context.UserName));
+
+            context.Validated(id);
+        }
+    }
+}
