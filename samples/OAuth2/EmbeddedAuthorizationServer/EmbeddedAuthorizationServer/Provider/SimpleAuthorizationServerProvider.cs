208c1c5213d8ac67df063db03089e3625be32dfc
Dominick Baier <dbaier@leastprivilege.com>
2013-11-13 11:14:12 +0100

    Moved the embedded RO sample to a different folder

--- samples/OAuth2/EmbeddedAuthorizationServer/EmbeddedAuthorizationServer/Provider/SimpleAuthorizationServerProvider.cs
+++ samples/OAuth2/EmbeddedAuthorizationServer/EmbeddedAuthorizationServer/Provider/SimpleAuthorizationServerProvider.cs
@@ -8,13 +8,15 @@ namespace EmbeddedAuthorizationServer.Provider
     {
         public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
-            // accept any client
+            // OAuth2 supports the notion of client authentication
+            // this is not used here
             context.Validated();
         }
 
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
             // validate user credentials
+            // user credentials should be stored securely
             if (context.UserName != context.Password)
             {
                 context.Rejected();
@@ -24,6 +26,7 @@ namespace EmbeddedAuthorizationServer.Provider
             // create identity
             var id = new ClaimsIdentity("Embedded");
             id.AddClaim(new Claim("sub", context.UserName));
+            id.AddClaim(new Claim("role", "user"));
 
             context.Validated(id);
         }
