108294df9500f15ed4a19e3cc1a6f204b48fb08e
Dominick Baier <dbaier@leastprivilege.com>
2013-11-15 08:36:48 +0100

    Added refresh token sample

---
+++ samples/OAuth2/EmbeddedResourceOwnerFlowWithRefreshTokens/EmbeddedAuthorizationServer/Provider/SimpleRefreshTokenProvider.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Infrastructure;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace EmbeddedAuthorizationServer.Provider
+{
+    // sample persistence of refresh tokens
+    // this is not production ready!
+    public class SimpleRefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        private static ConcurrentDictionary<string, AuthenticationTicket> _refreshTokens = new ConcurrentDictionary<string,AuthenticationTicket>();
+
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var guid = Guid.NewGuid().ToString();
+            
+            // maybe only create a handle the first time, then re-use for same client
+            _refreshTokens.TryAdd(guid, context.Ticket);
+
+            // consider storing only the hash of the handle
+            context.SetToken(guid);
+        }
+
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            AuthenticationTicket ticket;
+            if (_refreshTokens.TryRemove(context.Token, out ticket))
+            {
+                context.SetTicket(ticket);
+            }
+        }
+
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
